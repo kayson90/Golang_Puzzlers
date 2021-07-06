d3701c596102b52e90707b957efd242cd328c225
lvwenkai <lvwenkai@bytedance.com>
2021-07-06 13:10:43 +0800

    字典的key的要求
    Change-Id: Ice0a09d6333b8989fb33a6d42f7506c7b6da7b78

--- src/puzzlers/article9/q1/demo18.go
+++ src/puzzlers/article9/q1/demo18.go
@@ -1,11 +1,14 @@
 package main
 
+//实际上就是hash表，需要判断是否等于来操作
 func main() {
 	// 示例1。
+	// key是切片
 	//var badMap1 = map[[]int]int{} // 这里会引发编译错误。
 	//_ = badMap1
 
 	// 示例2。
+	// key是切片
 	//var badMap2 = map[interface{}]int{
 	//	"1":      1,
 	//	[]int{2}: 2, // 这里会引发panic。
@@ -14,10 +17,12 @@ func main() {
 	//_ = badMap2
 
 	// 示例3。
+	// 数组的元素不能为切片
 	//var badMap3 map[[1][]string]int // 这里会引发编译错误。
 	//_ = badMap3
 
 	// 示例4。
+	// 结构体的元素不能为切片
 	//type BadKey1 struct {
 	//	slice []string
 	//}
@@ -25,6 +30,7 @@ func main() {
 	//_ = badMap4
 
 	// 示例5。
+	// 多维数组的元素为切片，不管隐藏多深
 	//var badMap5 map[[1][2][3][]string]int // 这里会引发编译错误。
 	//_ = badMap5
 

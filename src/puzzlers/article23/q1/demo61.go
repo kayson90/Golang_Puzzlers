ac4741ca485e0485f8232c1417d0bc0343a27698
lvwenkai <lvwenkai@bytedance.com>
2021-11-28 15:58:12 +0800

    条件变量
    Change-Id: Ie7cb79dbf5666d4c54dc5ecbeaba172b17d820fe

--- src/puzzlers/article23/q1/demo61.go
+++ src/puzzlers/article23/q1/demo61.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
+//实际上就是signal和wait
+//就是为了让线程走走停停
+//条件等待
 func main() {
 	// mailbox 代表信箱。
 	// 0代表信箱是空的，1代表信箱是满的。
@@ -13,6 +16,7 @@ func main() {
 	// lock 代表信箱上的锁。
 	var lock sync.RWMutex
 	// sendCond 代表专用于发信的条件变量。
+	// 条件变量的初始化离不开互斥锁，并且它的方法有的也是基于互斥锁的。
 	sendCond := sync.NewCond(&lock)
 	// recvCond 代表专用于收信的条件变量。
 	recvCond := sync.NewCond(lock.RLocker())
@@ -27,13 +31,17 @@ func main() {
 		for i := 1; i <= max; i++ {
 			time.Sleep(time.Millisecond * 500)
 			lock.Lock()
+			//用for是因为可能多个线程被同时缓存，但是只有1个会往下走，其余的还是要继续等待
 			for mailbox == 1 {
+				//wait的时候会进行解锁，所以前面要锁定
 				sendCond.Wait()
+				//唤醒以后会重新进行锁定
 			}
 			log.Printf("sender [%d]: the mailbox is empty.", i)
 			mailbox = 1
 			log.Printf("sender [%d]: the letter has been sent.", i)
 			lock.Unlock()
+			//不需要用锁，并且一般在释放锁以后再通知
 			recvCond.Signal()
 		}
 	}(max)
